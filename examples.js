fc3bb6bb32e33d1e317888f4e76ba77db17193c5
LukaszKoziarski <lkoziars@gmail.com>
2017-10-02 20:36:44 +0200

    added numbers of examples

--- examples.js
+++ examples.js
@@ -15,12 +15,12 @@ console.log("setC: " + setC);
 console.log("-----------------");
 
 console.log("Union:");
-console.log("API.union(setA, setB) => ", API.union(setA, setB));
-console.log("setB.union(setC) => ", setB.union(setC));
+console.log("1. API.union(setA, setB) => ", API.union(setA, setB));
+console.log("2. setB.union(setC) => ", setB.union(setC));
 console.log("-----------------");
 
 console.log("Intersection:");
-console.log("API.intersection(setA, setB) => ", API.intersection(setA, setB));
-console.log("setB.intersection(setC) => ", setB.intersection(setC));
+console.log("1. API.intersection(setA, setB) => ", API.intersection(setA, setB));
+console.log("2. setB.intersection(setC) => ", setB.intersection(setC));
 console.log("-----------------");
 
